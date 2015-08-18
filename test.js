61d6680d8bd0bcfcdd611446cdbccc4cfafed844
Jan Święcki <jan.swiecki@gmail.com>
2015-08-18 04:07:08 +0200

    test.js

--- test.js
+++ test.js
@@ -1,43 +1,43 @@
 require("./index.js").then(function(rest){
   var log = require("./lib/SimpleLogger.js").getLogger();
 
-// Example
-// If you return string response gets Content-Type: text/plain automatically.
+  // Example
+  // If you return string response gets Content-Type: text/plain automatically.
   rest.get("/test", function() {
     return "return text/plain string";
   });
 
-// Automatic variable name mapping (order of arguments in function doesn't matter)
-// e.g. /user/1/name --> 1, name
+  // Automatic variable name mapping (order of arguments in function doesn't matter)
+  // e.g. /user/1/name --> 1, name
   rest.get("/user/:userId/:userProperty", function(userProperty, userId) {
     return userId+", "+userProperty;
   });
 
-// chaining
+  // chaining
   rest
-    // if you return simple object then the response
-    // has Content-Type: application/json automatically
+      // if you return simple object then the response
+      // has Content-Type: application/json automatically
       .get("/object", function () {
         return {msg: "return as application/json"};
       })
 
-    // automatic setting Content-Type via function name
+      // automatic setting Content-Type via function name
       .get("/pdf", function asApplicationPdf() {
         return {msg: "return as application/pdf"};
       })
-    // POST
+      // POST
       .post("/post", function asNonExistent() {
         return "why"; // Content-Type: non-existent
       })
 
-    // file download? no problem!
+      // file download? no problem!
       .get("/file", function(File) {
 
         return File("package.json");
 
       })
 
-    // auto-wire core module? not a problem!
+      // auto-wire core module? not a problem!
       .delete("/file/:name", function returnsStatusCode(fs, name) {
         if(! fs.existsSync(name)) {
           return 404;
@@ -47,11 +47,11 @@ require("./index.js").then(function(rest){
         }
       })
 
-    // DEPRECATED:
-    // asynchronous version of above
-    // Note: if Async is injected then framework automatically assumes
-    //       asynchronous callback will be called and ignores return
-    //       value of handler.
+      // DEPRECATED:
+      // asynchronous version of above
+      // Note: if Async is injected then framework automatically assumes
+      //       asynchronous callback will be called and ignores return
+      //       value of handler.
       .delete("/fileAsync/:name", function returnsStatusCode(fs, name, Async) {
         fs.exists(name, function(exists) {
 
@@ -68,10 +68,10 @@ require("./index.js").then(function(rest){
         });
       })
 
-    // asynchronous version of above
-    // Note: if Async is injected then framework automatically assumes
-    //       asynchronous callback will be called and ignores return
-    //       value of handler.
+      // asynchronous version of above
+      // Note: if Async is injected then framework automatically assumes
+      //       asynchronous callback will be called and ignores return
+      //       value of handler.
       .delete("/fileAsync/:name", function returnsStatusCode(fs, name) {
         return new Promise(function(resolve, reject){
           fs.exists(name, function(exists) {
