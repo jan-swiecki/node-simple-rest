ee6f226a6badc5824a752235ae6f353bbae03530
Jan Święcki <jan.swiecki@gmail.com>
2015-08-18 05:47:14 +0200

    Update

--- test/test.js
+++ test/test.js
@@ -1,3 +1,71 @@
-/**
- * Created by Gandalf on 2015-08-18.
- */
+require("./index.js").then(function(rest){
+	var log = require("./lib/SimpleLogger.js").getLogger();
+
+	// Example
+	// If you return string response gets Content-Type: text/plain automatically.
+	rest.get("/test", function() {
+		return "return text/plain string";
+	});
+
+	// Automatic variable name mapping (order of arguments in function doesn't matter)
+	// e.g. /user/1/name --> 1, name
+	rest.get("/user/:userId/:userProperty", function(userProperty, userId) {
+		return userId+", "+userProperty;
+	});
+
+	// chaining
+	rest
+		// if you return simple object then the response
+		// has Content-Type: application/json automatically
+		.get("/object", function () {
+			return {msg: "return as application/json"};
+		})
+
+		// automatic setting Content-Type via function name
+		.get("/pdf", function asApplicationPdf() {
+			return {msg: "return as application/pdf"};
+		})
+
+		// POST
+		.post("/post", function asNonExistent() {
+			return "why"; // Content-Type: non-existent
+		})
+
+		// file download? no problem!
+		.get("/file", function(File) {
+			return File("package.json");
+		})
+
+		// auto-wire core module? not a problem!
+		.delete("/file/:name", function returnsStatusCode(fs, name) {
+			if(! fs.existsSync(name)) {
+				return 404;
+			} else {
+				fs.unlinkSync(name);
+				return 200;
+			}
+		})
+
+		// asynchronous version of above
+		// Note: if Async is injected then framework automatically assumes
+		//       asynchronous callback will be called and ignores return
+		//       value of handler.
+		.delete("/fileAsync/:name", function returnsStatusCode(fs, name) {
+			return new Promise(function(resolve, reject){
+				fs.exists(name, function(exists) {
+
+					setTimeout(function(){
+						log("Unlink "+name);
+						if(! exists) {
+							resolve(404);
+						} else {
+							fs.unlinkSync(name);
+							resolve(200);
+						}
+					}, 1000);
+
+				});
+			});
+		});
+
+});
